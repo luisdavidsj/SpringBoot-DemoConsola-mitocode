b5b9c6cab1acd3645ee3446fdfcc67024b6e8f61
luisd <luisd@SpiderWolf>
2021-11-09 13:47:49 -0600

    Varias implementaciones, Qualifier

--- DemoConsola/src/main/java/com/mitocode/repository/PersonaRepoImpl2.java
+++ DemoConsola/src/main/java/com/mitocode/repository/PersonaRepoImpl2.java
@@ -2,18 +2,20 @@ package com.mitocode.repository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 import com.mitocode.DemoConsolaApplication;
 
 @Repository
-public class PersonaRepoImpl implements IPersonaRepo{
+@Qualifier("persona2")
+public class PersonaRepoImpl2 implements IPersonaRepo{
 	
 	private static Logger LOG = LoggerFactory.getLogger(DemoConsolaApplication.class);
 
 	@Override
 	public void registrar(String nombre) {
-		LOG.info("SE REGISTRO A " + nombre);
+		LOG.info("NO SE REGISTRO A " + nombre);
 	}
 
 }
--- DemoConsola/src/main/java/com/mitocode/service/PersonaServiceImpl.java
+++ DemoConsola/src/main/java/com/mitocode/service/PersonaServiceImpl.java
@@ -1,6 +1,7 @@
 package com.mitocode.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.mitocode.repository.IPersonaRepo;
@@ -9,6 +10,7 @@ import com.mitocode.repository.IPersonaRepo;
 public class PersonaServiceImpl implements IPersonaService{
 
 	@Autowired
+	@Qualifier("persona2")
 	private IPersonaRepo repo;
 	
 	@Override
