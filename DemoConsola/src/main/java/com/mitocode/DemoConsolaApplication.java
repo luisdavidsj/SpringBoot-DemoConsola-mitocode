6824d2561eecff830a32a8c741255bb9b67c4eb9
luisd <luisd@SpiderWolf>
2021-11-09 13:23:19 -0600

    Inyección de dependencias y estereotipos

--- DemoConsola/src/main/java/com/mitocode/DemoConsolaApplication.java
+++ DemoConsola/src/main/java/com/mitocode/DemoConsolaApplication.java
@@ -2,14 +2,19 @@ package com.mitocode;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.mitocode.service.IPersonaService;
 
 @SpringBootApplication
 public class DemoConsolaApplication implements CommandLineRunner{
 	
 	private static Logger LOG = LoggerFactory.getLogger(DemoConsolaApplication.class);
+	@Autowired
+	private IPersonaService service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoConsolaApplication.class, args);
@@ -18,8 +23,9 @@ public class DemoConsolaApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		//System.out.println("Hola Coders desde Spring Boot en consola");
-		LOG.info("Hola Coders desde Spring Boot en consola");
-		LOG.warn("Advirtiendo a los coders");
+		//LOG.info("Hola Coders desde Spring Boot en consola");
+		//LOG.warn("Advirtiendo a los coders");
+		service.registrar("MitoCode");
 	}
 
 	
